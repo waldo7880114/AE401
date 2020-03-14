3f57c7669b6745327a94ec78e57aeab75c6b41fa
alecwsadgmailcom <61896282+alecwsadgmailcom@users.noreply.github.com>
2020-03-14 09:47:45 +0800

    Add files via upload

---
+++ AE401_01.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  7 11:02:58 2020
+
+@author: user
+"""
+from mcpi.minecraft import  Minecraft
+mc=Minecraft.create()
+
+pos=mc.player.getTilePos()
+print(pos)
+
+x = -100
+y = 100
+z = 100
+
+mc.player.setTilePos(x,y,z)
